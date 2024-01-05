01d51088c45ef19b25ba7a18f1423b724bec6724
Firdous <firdousrahaman42@gmail.com>
2024-01-05 14:48:19 +0530

    Added code for index.js

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const url = require('url');
+const app = express();
+const path = require('path');
+const PORT = 1806;
+
+let userIsAuthorised = false;
+const pass = "iLoveCoding";
+
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/public/index.html'));
+})
+
+app.use((req, res, next) => {
+    const password = req.body.password;
+
+    console.log(req.body)
+    if (password === pass) {
+        userIsAuthorised = true;
+    }
+    next();
+})
+
+app.post('/check', (req, res) => {
+    if (userIsAuthorised) {
+        res.sendFile(path.join(__dirname + '/public/secret.html'));
+        userIsAuthorised = false;
+    }
+    else {
+        res.redirect('/');
+    }
+})
+
+
+
+app.listen(PORT, () => { console.log("Server Started Successfully") });
